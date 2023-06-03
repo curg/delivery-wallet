9b6f2a8b13ce45e6d533e96f138f129d2d49cdc7
minwoogramer <01082249606nmw@gmail.com>
2023-06-03 16:02:32 +0900

    feat:Modal V1 중간단계

--- front/src/components/buttons/ConnectWallet.tsx
+++ front/src/components/buttons/ConnectWallet.tsx
@@ -1,6 +1,7 @@
 import { ClickHandler } from "@/types";
-import { Wallet } from "../icons";
+
 import React from "react";
+import { Wallet } from "../icons/Wallet";
 
 type ConnectWalletProps = {
   onClick?: ClickHandler;
