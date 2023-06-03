3edbdebd4b45470189048fbe56aebb6572f2926f
scottXchoo <scott@a41.io>
2023-06-03 16:42:10 +0900

    feat: useConnectWallet.ts

---
+++ front/src/hooks/useConnectWallet.ts
@@ -0,0 +1,58 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+export const useConnectWallet = () => {
+  const [connected, setConnected] = useState(false);
+  const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Please install MetaMask!");
+      }
+
+      ethereum?.on("accountsChanged", (accounts: string[]) => {
+        if (accounts.length > 0) {
+          setAddress(accounts[0]);
+          setConnected(true);
+        } else {
+          setAddress("");
+          setConnected(false);
+        }
+      });
+
+      ethereum?.on("chainChanged", (chainId: string) => {
+        console.log("chainChanged", chainId);
+      });
+
+      const provider = new ethers.BrowserProvider(ethereum);
+
+      (async () => {
+        const accounts = await provider.listAccounts();
+        if (accounts.length > 0) {
+          setAddress(accounts[0].address);
+          setConnected(true);
+        }
+      })();
+
+      return () => {
+        ethereum.removeAllListeners("accountsChanged");
+        ethereum.removeAllListeners("chainChanged");
+      };
+    };
+
+    connectWallet();
+  }, []);
+
+  return {
+    connected,
+    address,
+  };
+};
