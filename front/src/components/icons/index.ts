e4933af6e38fb7de3afa7006b911dac8191726a7
Resister-boy <hassanpumped17@gmail.com>
2023-06-03 15:32:32 +0900

    refactor: add index.ts to import icon'

---
+++ front/src/components/icons/index.ts
@@ -0,0 +1,11 @@
+import { Ethereum } from "./Ethereum";
+import { Polygon } from "./Polygon";
+import { USDC } from "./USDC";
+import { USDT } from "./USDT";
+
+export {
+  Ethereum,
+  Polygon,
+  USDC,
+  USDT
+}
