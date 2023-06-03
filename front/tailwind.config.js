ef240b8a5eda401bee9e6d2bacc0deadcad7952d
scottXchoo <scott@a41.io>
2023-06-03 11:22:08 +0900

    design system

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -13,6 +13,25 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      transparent: "transparent",
+      black: "#000000",
+      white: "#ffffff",
+      purple: {
+        50: "#8247E5",
+        100: "#6B26DF",
+      },
+      yellow: {
+        50: "#FFF613",
+      },
+      dark: {
+        50: "#AAAAAA",
+        100: "#888888",
+        150: "#555555",
+        200: "#222222",
+      },
+    },
   },
+
   plugins: [],
 };
