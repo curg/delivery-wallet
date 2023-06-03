aadbab3ff0b6ecd4349641d25c54ddb6c94252e8
scottXchoo <scott@a41.io>
2023-06-04 03:20:07 +0900

    feat: postWalletAddress

--- front/src/components/aaAssetsBlock/AAAssetsContainer.tsx
+++ front/src/components/aaAssetsBlock/AAAssetsContainer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useCallback } from "react";
 import ConnectWallet from "../buttons/ConnectWallet";
 import { useRecoilState } from "recoil";
 import { walletStateAtom } from "@/states/globalAtom";
@@ -10,9 +10,26 @@ import {
   simpleAccountFactory,
 } from "@/api/wallet/config/constants";
 import { getSimpleAccount } from "@/api/wallet/getSimpleAccount";
+import { BASE_URL } from "@/constants";
 
 const AAAssetsContainer = () => {
-  const [{ signingKey }, setWalletState] = useRecoilState(walletStateAtom);
+  const [{ eoaWalletAddress, aaWalletAddress, signingKey }, setWalletState] =
+    useRecoilState(walletStateAtom);
+
+  const postWalletAddress = useCallback(async () => {
+    const fetchResult = await fetch(`${BASE_URL}/addAddress`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        eoaAddress: eoaWalletAddress,
+        aaAddress: aaWalletAddress,
+      }),
+    });
+
+    return fetchResult;
+  }, [eoaWalletAddress, aaWalletAddress]);
 
   const handleCreateWallet = async () => {
     try {
@@ -29,7 +46,8 @@ const AAAssetsContainer = () => {
         ...prevState,
         aaWalletAddress: address,
       }));
-      return address;
+
+      await postWalletAddress();
     } catch (error) {
       console.log("Error creating wallet");
     }
