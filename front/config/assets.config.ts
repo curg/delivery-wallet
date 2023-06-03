d324ee575bac4fa97df89bfb80b4065fb36ad84f
scottXchoo <scott@a41.io>
2023-06-04 04:33:27 +0900

    WIP: approve

--- front/config/assets.config.ts
+++ front/config/assets.config.ts
@@ -3,24 +3,28 @@ export const assets = [
     ticker: "CURG",
     network: "CURG",
     amount: 20.1312,
-    tokenId: 1
+    tokenId: 1,
+    tokenAddress: "0xB186887176E450bFfc03697b0684347f3f346F3D",
   },
   {
     ticker: "USDT",
     network: "USDT",
     amount: 120.122,
-    tokenId: 2
+    tokenId: 2,
+    tokenAddress: "0x0d1FCB66050EE7a14FF4B9a2bf499f521BbA41bD",
   },
   {
     ticker: "WBTC",
     network: "WBTC",
     amount: 94.294,
-    tokenId: 3
+    tokenId: 3,
+    tokenAddress: "0xac5058E9Ac8b8aD7A7B765E360FfA7943F530be5",
   },
   {
     ticker: "DAI",
     network: "DAI",
     amount: 342.203,
-    tokenId: 4
+    tokenId: 4,
+    tokenAddress: "0x236F53345F541c755DF70988C7E8e4b658B820aA",
   },
 ];
