a20245cef87c976f1f1e29f31ab879194ea0d3bf
Resister-boy <hassanpumped17@gmail.com>
2023-06-03 12:24:59 +0900

    feat: create test.tsx

---
+++ front/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ETH } from '@/components/icons'
+
+const Test = () => {
+  return (
+    <div>
+      <ETH className='' />      
+    </div>
+  )
+}
+
+export default Test
