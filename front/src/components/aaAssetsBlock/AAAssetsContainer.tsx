183076899e3259a476440a0654723afab0a9ddb5
scottXchoo <scott@a41.io>
2023-06-03 19:03:27 +0900

    feat: ConnectWallet.tsx

--- front/src/components/aaAssetsBlock/AAAssetsContainer.tsx
+++ front/src/components/aaAssetsBlock/AAAssetsContainer.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import ConnectWallet from "../buttons/ConnectWallet";
 
 const AAAssetsContainer = () => {
   return (
-    <div className='min-h-[450px] mt-8 flex justify-center items-center rounded-lg bg-purple-50'>
-      Create Wallet
+    <div className="min-h-[450px] mt-8 flex justify-center items-center rounded-lg bg-purple-50">
+      <ConnectWallet content="Create AA Wallet" />
     </div>
-  )
-}
+  );
+};
 
-export default AAAssetsContainer
+export default AAAssetsContainer;
